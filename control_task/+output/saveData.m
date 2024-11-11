47a7e27e18c16ca1323efb9a8eeb78503bda3684
Katharina Wellstein <33021795+kwellstein@users.noreply.github.com>
2024-11-11 11:12:25 +1100

    saving diary as txt file into participant folders, deleted old diary

--- control_task/+output/saveData.m
+++ control_task/+output/saveData.m
@@ -37,6 +37,5 @@ save(fullfile([options.files.savePath,'/',options.files.dataFileName]),'dataFile
 save(fullfile([options.files.savePath,'/',options.files.optionsFileName]),'options');
 
 diary off
-save(fullfile([options.files.savePath,'/diary']));
-
+save(fullfile([options.files.savePath,'/diary.txt']));
 end
